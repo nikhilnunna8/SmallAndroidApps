b3a42230bc8efe8a3f2be30ae669f2fbb414638c
nik807 <122914471+nikhilnunna8@users.noreply.github.com>
2024-08-28 06:33:54 -0500

    Created the BusinessCardText Function
    Created the function to display the business photo, icons, and text in a formatted manner using columns.

--- BusinessCard/app/src/main/java/com/example/businesscardnikhil/MainActivity.kt
+++ BusinessCard/app/src/main/java/com/example/businesscardnikhil/MainActivity.kt
@@ -4,13 +4,30 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Call
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.businesscardnikhil.ui.theme.BusinessCardNikhilTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,9 +37,12 @@ class MainActivity : ComponentActivity() {
         setContent {
             BusinessCardNikhilTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
+                    BusinessCardText(
                         name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                        job = "king",
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .fillMaxSize()
                     )
                 }
             }
@@ -37,11 +57,56 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         modifier = modifier
     )
 }
+@Composable
+fun BusinessCardText(name: String, job: String, modifier: Modifier = Modifier){
+    Box(
+        modifier = Modifier
+            .padding(10.dp)
+            .fillMaxSize()
+
+    ) {
+
+        Column(
+            verticalArrangement = Arrangement.Bottom,
+            modifier = Modifier
+        ) {
+            val image = painterResource(R.drawable._000004659)
+            Image(
+                painter = image,
+                contentDescription = null,
+                modifier = Modifier
+                    .width(500.dp)
+                    .padding(top = 50.dp, bottom = 100.dp)
+            )
+            Icon(
+                Icons.Default.Call,
+                contentDescription = null
+            )
+            Text(
+                text = name,
+                fontSize = 33.sp,
+                textAlign = TextAlign.Center,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            )
+            Text(
+                text = job,
+                fontSize = 20.sp,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .padding(5.dp)
+                    .fillMaxWidth()
+            )
+        }
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     BusinessCardNikhilTheme {
-        Greeting("Android")
+        BusinessCardText("Nikhil Nunna", "High School Student")
     }
 }
