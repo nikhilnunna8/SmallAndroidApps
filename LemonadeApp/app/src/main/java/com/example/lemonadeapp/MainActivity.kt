908b30bea933355593cf203ad69a035e3357aa14
Lakshmi Nunna <nunna8021@students.d211.org>
2024-09-06 08:19:43 -0500

    Created the LemonAppFunctionality function
    Created a function that changes the value of a number corresponding to a picture

--- LemonadeApp/app/src/main/java/com/example/lemonadeapp/MainActivity.kt
+++ LemonadeApp/app/src/main/java/com/example/lemonadeapp/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
@@ -17,6 +18,15 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.lemonadeapp.ui.theme.LemonadeAppTheme
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
+import java.io.StringReader
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,7 +37,9 @@ class MainActivity : ComponentActivity() {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     LemonApp(
                         modifier = Modifier
-                            .fillMaxSize()
+                            .fillMaxSize(),
+                        imageResourceId = R.drawable.lemon_tree,
+                        stringResourceId = R.drawable.lemon_tree
                     )
                 }
             }
@@ -35,23 +47,47 @@ class MainActivity : ComponentActivity() {
     }
 }
 @Composable
-fun LemonApp(modifier: Modifier = Modifier, imageResourceId: Int, ) {
+fun LemonApp(modifier: Modifier = Modifier, imageResourceId: Int, stringResourceId: Int) {
     val imageResource = imageResourceId
-    Column() {
-        Image(
-            painter = painterResource(imageResource),
-            contentDescription = stringResource(R.string.lemonTree)
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Button(
+            onClick = { /*TEST*/ },
+            shape = RoundedCornerShape(30.dp)
         )
-        Button(onClick = { /*TEST*/ })
         {
-
+            Image(
+                painter = painterResource(imageResource),
+                contentDescription = null
+            )
         }
+        Text(
+            text = stringResource(stringResourceId)
+        )
     }
 }
 
+@Composable
+fun LemonAppFunctionality(modifier: Modifier){
+    var imageRes by remember { mutableStateOf(1) }
+    var count = 0
+    val imageResource = when(imageRes) {
+        1 -> R.drawable.lemon_tree
+        2 -> R.drawable.lemon_squeeze
+        3 -> R.drawable.lemon_drink
+        else -> R.drawable.lemon_restart
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
 fun LemonAppPreview() {
-    LemonApp(modifier = Modifier, imageResourceId = R.drawable.lemon_tree)
+    LemonApp(
+        modifier = Modifier.fillMaxSize(),
+        imageResourceId = R.drawable.lemon_tree,
+        stringResourceId = R.string.lemonTree
+    )
 }
