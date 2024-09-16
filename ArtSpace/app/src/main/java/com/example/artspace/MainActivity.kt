9f457a28472855f70b7f698431a694c349fe1ea3
Lakshmi Nunna <nunna8021@students.d211.org>
2024-09-16 08:19:59 -0500

    Created ArtSpace layout and added string and image resources
    Added resources and created the layout of the app

--- ArtSpace/app/src/main/java/com/example/artspace/MainActivity.kt
+++ ArtSpace/app/src/main/java/com/example/artspace/MainActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.artspace.ui.theme.ArtSpaceTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,9 +30,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             ArtSpaceTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    ArtSpace(
+                        modifier = Modifier.fillMaxSize()
                     )
                 }
             }
@@ -31,17 +40,44 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ArtSpace(modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        Image(
+            painter = painterResource(R.drawable.london),
+            contentDescription = null
+        )
+        Row(){
+            Column() {
+                Text(text = stringResource(R.string.app_name))
+                Text(text = stringResource(R.string.app_name))
+            }
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Row() {
+            ArtSpaceButtons()
+        }
+    }
+}
+
+@Composable
+fun ArtSpaceButtons() {
+    Button(onClick = {/*TODO*/}
+    ) {
+        Text(text = stringResource(R.string.app_name))
+    }
+    Button(onClick = {/*TODO*/}
+    ) {
+        Text(text = stringResource(R.string.app_name))
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun ArtSpacePreview() {
     ArtSpaceTheme {
-        Greeting("Android")
+        ArtSpace(
+            modifier = Modifier
+                .fillMaxSize()
+        )
     }
 }
